5366d2a484583a9206a8c6924302f0ac93e0e8ea
Dominique Skaf <doskaf@yahoo.com>
2019-08-28 04:13:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -194,13 +194,15 @@ end
 
 def big_shoe_rebounds
   biggest_shoe = 0
+  rebounds = 0
   game_hash.each do |place, team|
     team[:players].each do |player|
       shoe_size = player[:shoe]
       if shoe_size > biggest_shoe
         biggest_shoe = shoe_size
-        return player[:rebounds]
+        rebounds = player[:rebounds]
       end
     end
   end
+  rebounds
 end
