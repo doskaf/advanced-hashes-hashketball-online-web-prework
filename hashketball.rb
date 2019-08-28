865fe64fcf5e73202ae1a0a17b734076855ff18c
Dominique Skaf <doskaf@yahoo.com>
2019-08-28 04:19:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -213,9 +213,7 @@ def most_points_scored
   most_points = 0
   game_hash.each do |place, team|
     team[:players].each do |player|
-      points = player[:points]
-      if points > most_points
-        most_points = points
+      if player[:points] > most_points
         return player[:player_name]
       end
     end
