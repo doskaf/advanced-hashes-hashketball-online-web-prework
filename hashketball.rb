9e9e071fb10f9604f017341bbf1f1fc3f5172e3f
Dominique Skaf <doskaf@yahoo.com>
2019-08-28 04:52:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -243,9 +243,9 @@ def player_with_longest_name
   longest_name = 0
   game_hash.each do |place, team|
     team[:players].each do |player|
-      name = player[:player_name]
-      if name.length > longest_name
-        longest_name = name
+      name = player[:player_name].length
+      if name > longest_name
+        longest_name = player[:player_name]
       end
     end
   end
