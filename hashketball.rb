f1482d1439c7fa142811a100aea7f0a02d1c0b92
Dominique Skaf <doskaf@yahoo.com>
2019-08-28 03:33:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -182,9 +182,8 @@ def player_stats(player_name)
   game_hash.each do |place, team| 
     team.each do |attribute, data| 
       if data.include?(player_name) 
-       return [:players].delete_if {|k, v| 
+       return [:players].delete_if {|k, v| k == :player_name}
       end
     end
   end
-  stats
 end
